aebfd78454de4bd2255fd0d412e0bc1bad8f35c5
JesusIgnacio <ignacio.casbar@gmail.com>
2019-11-08 11:15:14 -0300

    add: add cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const apiRoutes = require("./api-routes")
-
+var cors = require('cors');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
 app.use('/api', apiRoutes)
 
 app.listen(PORT, () => {
